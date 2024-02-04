8c663c2e957d45eeb3bf294adca1a6ddfb8d5ccd
heba <sketch@sketch-tech.com>
2024-02-04 15:30:23 +0200

    first insall jwt

---
+++ app/Http/Controllers/Api/ApiController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class ApiController extends Controller
+{
+    //Register API [post ,formdata]
+    public function register(){
+
+    }
+    //login API [POst ,formdata]
+    public function login(){
+
+    }
+    //profile API (GET)
+    public function profile (){
+
+    }
+     //profile API (GET)
+
+    public function refreshToken (){
+
+    }
+    //Logout APi(get)
+    public function logout(){
+
+    }
+}
